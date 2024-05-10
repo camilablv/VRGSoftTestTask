7bfc2418a0c5b1d1af1b99b6070ea79f48a5e282
Camila <camilaabalova@gmail.com>
2024-05-10 15:45:21 +0300

    save state

--- app/src/main/java/com/ca/vrgsofttesttask/presentation/screens/topposts/TopPostsScreen.kt
+++ app/src/main/java/com/ca/vrgsofttesttask/presentation/screens/topposts/TopPostsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -20,15 +21,19 @@ fun TopPostsScreen(
     navigateToFullScreenImage: (String) -> Unit
 ) {
 
+    val listState = rememberLazyListState()
+
     val viewState = viewModel.viewState.collectAsStateWithLifecycle()
-    val posts = viewModel.posts().collectAsLazyPagingItems()
+    val posts = viewState.value.posts.collectAsLazyPagingItems()
     val context = LocalContext.current
-    
+
+
     LazyColumn(
         modifier = Modifier
             .fillMaxSize(),
         contentPadding = PaddingValues(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        state = listState
     ) {
         items(posts.itemCount) { index ->
             posts[index]?.let { post ->
--- app/src/main/java/com/ca/vrgsofttesttask/presentation/screens/topposts/TopPostsScreenState.kt
+++ app/src/main/java/com/ca/vrgsofttesttask/presentation/screens/topposts/TopPostsScreenState.kt
@@ -1,7 +1,10 @@
 package com.ca.vrgsofttesttask.presentation.screens.topposts
 
+import androidx.paging.PagingData
 import com.ca.vrgsofttesttask.domain.model.Post
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
 data class TopPostsScreenState(
-    val posts: List<Post> = listOf()
+    val posts: Flow<PagingData<Post>> = flowOf()
 )
